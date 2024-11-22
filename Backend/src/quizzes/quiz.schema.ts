3bd5bf7b0bbbb315315b8d52cc2ab2361e08097f
janafouda <jana.fouda11@gmail.com>
2024-11-22 22:25:29 +0200

    Update quiz.schema.ts

--- Backend/src/quizzes/quiz.schema.ts
+++ Backend/src/quizzes/quiz.schema.ts
@@ -1,1 +1,24 @@
-//schema test
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import mongoose, { HydratedDocument } from 'mongoose';
+
+@Schema()
+export class quizzes {
+  
+  @Prop({ type: String, required: true, unique: true })
+  quiz_id: string; 
+  
+  @Prop({ type: String, required: true, unique: true })
+  module_id: string; 
+  
+  @Prop({ type: [{ question: String, options: [String], correct_answer: String }], required: true })
+  questions: { 
+    question: string; 
+    options: string[]; 
+    correct_answer: string; 
+  }[];
+  
+  @Prop({ type: Date, default: () => new Date() })
+  created_at: Date; 
+}
+
+export const QuizSchema = SchemaFactory.createForClass(quizzes);
