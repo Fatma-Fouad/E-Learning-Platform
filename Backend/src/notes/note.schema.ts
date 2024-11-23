0580ace5bdd444df6a5079e1236e0a59b80f5c0e
malakfouadd <malaksayhi35@gmail.com>
2024-11-23 23:01:21 +0400

    note,schema.ts Update

--- Backend/src/notes/note.schema.ts
+++ Backend/src/notes/note.schema.ts
@@ -1,24 +1,28 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { Document } from 'mongoose';
+import mongoose, { HydratedDocument } from 'mongoose';
+//import { courses } from '../../courses/course.schema.ts';
+//import { users } from '../users/user.schema.ts';
 
 @Schema({ timestamps: true })
-export class Note extends Document {
-  @Prop({ required: true })
-  noteId: string;
+export class Note {
 
-  @Prop({ required: true })
-  userId: string;
+  @Prop({ type: mongoose.Schema.Types.ObjectId, required: true, unique: true })
+  noteId: mongoose.Schema.Types.ObjectId;
 
-  @Prop()
-  courseId?: string;
+  @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'users', required: true, unique: true })
+  user_id: mongoose.Schema.Types.ObjectId;
+
+
+  @Prop({type: mongoose.Schema.Types.ObjectId, ref: 'courses', required: true , unique: true})
+  courseId: mongoose.Schema.Types.ObjectId;;
 
   @Prop({ required: true })
   content: string;
 
-  @Prop()
+  @Prop({type: Date, default: () => new Date()})
   createdAt: Date;
 
-  @Prop()
+  @Prop({type: Date, default: () => new Date()})
   lastUpdated: Date;
 }
 
