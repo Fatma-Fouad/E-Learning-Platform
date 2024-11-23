e17bcd06b91957cfacf4be46a73e34fb48c4d28a
Sandy Sadek <sandy.nsadek@gmail.com>
2024-11-23 20:19:27 +0200

    response schema-done
    updated 1.user_id
    2.quizzes_id
    
    imports

--- Backend/src/responses/response.schema.ts
+++ Backend/src/responses/response.schema.ts
@@ -1,7 +1,12 @@
 /* eslint-disable prettier/prettier */
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import mongoose, { HydratedDocument } from 'mongoose';
+<<<<<<< Updated upstream
 //import { Quiz } from '../../quizzes/models/quiz.schema';
+=======
+//import { quizzes } from '../quizzes/quiz.schema.ts';
+//import { User } from '../users/user.schema.ts';
+>>>>>>> Stashed changes
 
 export type ResponseDocument = HydratedDocument<Response>;
 
@@ -10,10 +15,17 @@ export class Response {
   @Prop({ type: mongoose.Schema.Types.ObjectId, required: true, unique: true })
   response_id: mongoose.Schema.Types.ObjectId;
 
+<<<<<<< Updated upstream
   @Prop({ type: mongoose.Schema.Types.ObjectId, required: true })
   user_id: mongoose.Schema.Types.ObjectId;
 
   @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'Quiz', required: true })
+=======
+  @Prop({ type: mongoose.Schema.Types.ObjectId,ref:'User', required: true,unique: true })
+  user_id: mongoose.Schema.Types.ObjectId;
+
+  @Prop({ type: mongoose.Schema.Types.ObjectId, ref:'quizzes', required: true,unique: true })
+>>>>>>> Stashed changes
   quiz_id: mongoose.Schema.Types.ObjectId;
 
   @Prop({ type: [{ question_id: String, selected_option: String }], required: true })
@@ -30,3 +42,7 @@ export class Response {
 }
 
 export const ResponseSchema = SchemaFactory.createForClass(Response);
+<<<<<<< Updated upstream
+=======
+
+>>>>>>> Stashed changes
