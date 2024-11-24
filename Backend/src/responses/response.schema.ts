684e8c9ca1c19dcc4f18d481ab38f88d989b54de
janafouda <jana.fouda11@gmail.com>
2024-11-24 13:43:59 +0200

    Update response.schema.ts
    added hydrateddocument

--- Backend/src/responses/response.schema.ts
+++ Backend/src/responses/response.schema.ts
@@ -1,48 +1,29 @@
-/* eslint-disable prettier/prettier */
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import mongoose, { HydratedDocument } from 'mongoose';
-<<<<<<< Updated upstream
-//import { Quiz } from '../../quizzes/models/quiz.schema';
-=======
-//import { quizzes } from '../quizzes/quiz.schema.ts';
-//import { User } from '../users/user.schema.ts';
->>>>>>> Stashed changes
+//import { quizzes } from '../../quizzes/quiz.schema.ts';
 
-export type ResponseDocument = HydratedDocument<Response>;
+export type ResponseDocument = HydratedDocument<responses>;
 
 @Schema()
-export class Response {
-  @Prop({ type: mongoose.Schema.Types.ObjectId, required: true, unique: true })
-  response_id: mongoose.Schema.Types.ObjectId;
-
-<<<<<<< Updated upstream
+export class responses {
+  
   @Prop({ type: mongoose.Schema.Types.ObjectId, required: true })
   user_id: mongoose.Schema.Types.ObjectId;
 
-  @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'Quiz', required: true })
-=======
-  @Prop({ type: mongoose.Schema.Types.ObjectId,ref:'User', required: true,unique: true })
-  user_id: mongoose.Schema.Types.ObjectId;
-
-  @Prop({ type: mongoose.Schema.Types.ObjectId, ref:'quizzes', required: true,unique: true })
->>>>>>> Stashed changes
+  @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'quizzes', required: true })
   quiz_id: mongoose.Schema.Types.ObjectId;
 
   @Prop({ type: [{ question_id: String, selected_option: String }], required: true })
   answers: {
-    question_id: string;
-    selected_option: string;
+    question_id: String;
+    selected_option: String;
   }[];
 
   @Prop({ type: Number, required: true })
-  score: number;
+  score: Number;
 
   @Prop({ type: Date, default: () => new Date(), required: true })
   submitted_at: Date;
 }
 
-export const ResponseSchema = SchemaFactory.createForClass(Response);
-<<<<<<< Updated upstream
-=======
-
->>>>>>> Stashed changes
+export const ResponseSchema = SchemaFactory.createForClass(responses);
