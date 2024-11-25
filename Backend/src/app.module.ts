15b3b26ea2dce3b370fa6c46deaef4a41ce35476
janafouda <jana.fouda11@gmail.com>
2024-11-25 19:09:21 +0200

    Update app.module.ts
    added mongoose connection

--- Backend/src/app.module.ts
+++ Backend/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [  
+    MongooseModule.forRoot('mongodb+srv://softwareproject:sp123@clustersp.wvdbq.mongodb.net/') ],
   controllers: [AppController],
   providers: [AppService],
 })
