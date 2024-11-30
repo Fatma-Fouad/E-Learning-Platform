8e81405448f0ead370dd73864f5d5e0be21f6710
RanaFathy85 <rana.abdullah@student.giu-uni.de>
2024-11-30 21:23:49 +0200

    course_management api update

--- Backend/src/progress/progress.schema.ts
+++ Backend/src/progress/progress.schema.ts
@@ -28,6 +28,7 @@ export class progress {
 
   @Prop({ type: Number, min: 0, max: 100, default: null })
   last_quiz_score: number | null;
+
 }
 
 export const ProgressSchema = SchemaFactory.createForClass(progress);
