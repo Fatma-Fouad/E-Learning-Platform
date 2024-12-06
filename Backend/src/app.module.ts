69e683987573d90501931e5f1d3d394bc0804440
Fatma-Fouad <fatma169f@gmail.com>
2024-12-06 16:25:46 +0200

    backup-done

--- Backend/src/app.module.ts
+++ Backend/src/app.module.ts
@@ -8,11 +8,13 @@ import { UserModule } from './users/user.module';
 import { ProgressModule } from './progress/models/progress.module';
 import { AuthModule } from './authentication/auth.module';
 import { AuthGuard } from './authentication/auth.guard';
+import { BackupService } from './backup/backup.service';
+import { BackupController } from './backup/backup.controller';
 
 @Module({
   imports: [ QuizModule, QuestionBankModule, UserModule, ProgressModule, AuthModule, 
     MongooseModule.forRoot('mongodb+srv://softwareproject:sp123@clustersp.wvdbq.mongodb.net/EduLink') ],
-    controllers: [AppController],
-  providers: [AppService, AuthGuard],
+    controllers: [AppController, BackupController],
+  providers: [AppService, AuthGuard, BackupService],
 })
 export class AppModule {}
